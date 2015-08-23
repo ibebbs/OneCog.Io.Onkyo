7c1f20f846352629efdbf0c42c9ecf7ddfcd91ff
Ian Bebbington <github@bebbs.co.uk>
2015-08-23 09:07:13 +0100

    Added GetPowerState command to zones 1 and 2

---
+++ src/OneCog.Io.Onkyo/Messages/Zone1/GetPowerState.cs
@@ -0,0 +1,21 @@
+﻿using OneCog.Io.Onkyo.Common;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace OneCog.Io.Onkyo.Messages.Zone1
+{
+    public class GetPowerState : ICommand<PowerState>
+    {
+        private const string CommandString = "PWRQSTN";
+
+        public Task<Fallible<PowerState>> Send(ICommandStream stream)
+        {
+            return stream.Send<PowerState>(
+                CommandString,
+                responses => responses
+                    .OfType<PowerStateResponse>()
+                    .Select(response => response.PowerState)
+            );
+        }
+    }
+}
---
+++ src/OneCog.Io.Onkyo/Messages/Zone2/GetPowerState.cs
@@ -0,0 +1,21 @@
+﻿using OneCog.Io.Onkyo.Common;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace OneCog.Io.Onkyo.Messages.Zone2
+{
+    public class GetPowerState : ICommand<PowerState>
+    {
+        private const string CommandString = "ZPWQSTN";
+
+        public Task<Fallible<PowerState>> Send(ICommandStream stream)
+        {
+            return stream.Send<PowerState>(
+                CommandString,
+                responses => responses
+                    .OfType<PowerStateResponse>()
+                    .Select(response => response.PowerState)
+            );
+        }
+    }
+}
