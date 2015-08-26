cfed11a099bc223d015b31e2a953f3d13ba3b101
Ian Bebbington <github@bebbs.co.uk>
2015-08-26 17:02:39 +0100

    Reverted to local nuget instance as can't get environment nuget to work

--- build.bat
+++ build.bat
@@ -1,12 +1,7 @@
 @echo Off
 
-set nuget=
-if "%nuget%" == "" (
-    set nuget=nuget
-)
-
 echo BUILD.BAT - NuGet package restore started.
-"%nuget%" restore ".\src\OneCog.Io.Onkyo.sln" -OutputDirectory ".\src\packages"
+".\src\.nuget\NuGet.exe" restore ".\src\OneCog.Io.Onkyo.sln" -OutputDirectory ".\src\packages"
 echo BUILD.BAT - NuGet package restore finished.
 
 echo BUILD.BAT - FAKE build started.
