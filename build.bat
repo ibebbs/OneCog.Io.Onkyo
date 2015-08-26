333f28179fce59aed3d774385ff093f767419830
Ian Bebbington <github@bebbs.co.uk>
2015-08-26 16:56:44 +0100

    Additional nuget source caused build fail. Removed.

--- build.bat
+++ build.bat
@@ -6,8 +6,7 @@ if "%nuget%" == "" (
 )
 
 echo BUILD.BAT - NuGet package restore started.
-%nuget% source Add -Name OneCog -Source https://www.myget.org/F/onecog/api/v2
-%nuget% restore ".\src\OneCog.Io.Onkyo.sln" -OutputDirectory ".\src\packages"
+"%nuget%" restore ".\src\OneCog.Io.Onkyo.sln" -OutputDirectory ".\src\packages"
 echo BUILD.BAT - NuGet package restore finished.
 
 echo BUILD.BAT - FAKE build started.
